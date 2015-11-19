a39ddfefac75425bab2d2a16a18a94525d7ca313
Jaeun Choi <jaeun12.choi@samsung.com>
2015-11-19 15:20:22 +0900

    Floatingbutton: fix eo file according to efl1.16 upgrade
    Change-Id: Iebb25508c5de355ae009b4d1d948b3f1e4068a69

--- mobile/src/lib/eext_floatingbutton.c
+++ mobile/src/lib/eext_floatingbutton.c
@@ -466,13 +466,15 @@ eext_floatingbutton_add(Evas_Object *parent)
    return obj;
 }
 
-EOLIAN static void
+EOLIAN static Eo *
 _eext_floatingbutton_eo_base_constructor(Eo *obj, Eext_Floatingbutton_Data *sd EINA_UNUSED)
 {
-   eo_do_super(obj, MY_CLASS, eo_constructor());
+   obj = eo_do_super_ret(obj, MY_CLASS, obj, eo_constructor());
    eo_do(obj,
          evas_obj_type_set(MY_CLASS_NAME_LEGACY),
          evas_obj_smart_callbacks_descriptions_set(_smart_callbacks));
+
+   return obj;
 }
 
 EOLIAN static void
